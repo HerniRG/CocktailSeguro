cf2bd4a1d29dd87eb58ec0e980f73a9249744f48
hernirg <hernanrg85@gmail.com>
2025-01-12 16:57:54 +0100

    Initial Commit

---
+++ CocktailSeguro/CocktailSeguroApp.swift
@@ -0,0 +1,17 @@
+//
+//  CocktailSeguroApp.swift
+//  CocktailSeguro
+//
+//  Created by Hernán Rodríguez on 12/1/25.
+//
+
+import SwiftUI
+
+@main
+struct CocktailSeguroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
